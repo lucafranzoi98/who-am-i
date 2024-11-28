fe2b83bb7175ae902d7f27dea56f7c4b9e633676
lucafranzoi98 <lucafranzoi98@gmail.com>
2024-11-28 15:51:17 +0100

    comment base in config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), glsl()],
-  base: '/who-am-i/',
+  // base: '/who-am-i/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
